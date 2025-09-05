45d4140bd3df45d88753fb0b912b39c4b82f3fbe
zhidkovm-cloud <zhidkov.m@gmail.com>
2025-09-06 02:56:38 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'durak-cache-v9-2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './sw-helper.js',
+  './icons/pwa-192.png',
+  './icons/pwa-512.png',
+  './icons/apple-touch-icon.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone(); caches.open(CACHE).then(cache => cache.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(()=>cached))
+  );
+});
